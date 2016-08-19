f2f8e68267262ba31c7e4d0b32299cc76e3bd3a4
Nima Tahami <nimat6462@yahoo.ca>
2016-08-19 17:52:14 -0400

    Color Scheme option working with Example, rest will follow shortly.

--- Example/FCAlertView/ExampleViewController.m
+++ Example/FCAlertView/ExampleViewController.m
@@ -25,51 +25,63 @@
     // LIST OF ALL CUSTOMIZATION YOU CAN MAKE
     
     _alertViewOptions = [[NSMutableArray alloc] init];
-    
+
     NSArray *options = @[@{@"title" : @"Color Scheme",
                            @"description" : @"Choose your own color scheme or from one of included.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @1,
+                           @"selection" : @[@"OFF", @"Turquoise", @"Green", @"Blue", @"Midnight", @"Purple", @"Orange", @"Red", @"Silver", @"Gray"]},
                          @{@"title" : @"Title Color",
                            @"description" : @"Choose your own color scheme or from one of included.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Subtitle Color",
                            @"description" : @"Choose your own color scheme or from one of included.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Rounded Corners",
                            @"description" : @"Choose rounding of the alert.",
                            @"setting" : @"18",
-                           @"status" : @1},
+                           @"status" : @1,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Hide Alert Title",
                            @"description" : @"Show or Hide the alert's title",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Alert Type",
                            @"description" : @"Choose from Pre-Set Success, Caution, and Warning.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Number of Buttons",
                            @"description" : @"You can have upto 2 extra buttons in your alert.",
                            @"setting" : @0,
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Auto-Close",
                            @"description" : @"Alert will hide after a certain time.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Outside Touch",
                            @"description" : @"Alert will hide after outside the alert is touched.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Hide Done Button",
                            @"description" : @"Hide the Done Button that closes the alert.",
                            @"setting" : @"OFF",
-                           @"status" : @0},
+                           @"status" : @0,
+                           @"customIndicator" : @0},
                          @{@"title" : @"Hide All Buttons",
                            @"description" : @"Hide all buttons that are in the alert.",
                            @"setting" : @"OFF",
-                           @"status" : @0}];
+                           @"status" : @0,
+                           @"customIndicator" : @0}];
     
     _alertViewOptions = [options mutableCopy];
     
@@ -120,10 +132,18 @@
         CAShapeLayer *circleLayer = [CAShapeLayer layer];
         [circleLayer setPath:[[UIBezierPath bezierPathWithOvalInRect:CGRectMake(self.tableView.frame.size.width - 20, 80/2 - 8/2, 8, 8)] CGPath]];
         
+        if (![[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"customIndicator"]) {
         if ([[[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"status"] isEqual:@0])
             [circleLayer setFillColor:[_redColor CGColor]];
         else
             [circleLayer setFillColor:[_greenColor CGColor]];
+        } else {
+            NSString *selectedColor = [[[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"selection"]
+                                       objectAtIndex:[[[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"status"] integerValue]];
+            
+            [circleLayer setFillColor:[[self checkFlatColors:selectedColor] CGColor]];
+            
+        }
 
         // Option Title Label
         
@@ -145,12 +165,15 @@
         
         // Option Setting Label
         
+        NSString *selectedSetting = [[[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"selection"]
+                                   objectAtIndex:[[[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"status"] integerValue]];
+        
         UILabel *optionSetting = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.tableView.frame.size.width - 30.0f, 80.0f)];
         optionSetting.numberOfLines = 1;
         optionSetting.font = [UIFont systemFontOfSize:16.0f weight:UIFontWeightRegular];
         optionSetting.textColor = [UIColor colorWithWhite:80.0f/255.0f alpha:1.0];
         optionSetting.textAlignment = NSTextAlignmentRight;
-        optionSetting.text = [NSString stringWithFormat:@"%@", [[_alertViewOptions objectAtIndex:indexPath.row] objectForKey:@"setting"]];
+        optionSetting.text = selectedSetting;
 
         // Adding Cell Subviews
         
@@ -168,7 +191,53 @@
 
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    NSMutableDictionary *selectedCell = [[_alertViewOptions objectAtIndex:indexPath.row] mutableCopy];
+    
+    NSInteger status = [[selectedCell objectForKey:@"status"] integerValue];
+    NSArray *selection = [selectedCell objectForKey:@"selection"];
+    
+    status = (status + 1) %(selection.count);
+    
+    [selectedCell setObject:[NSNumber numberWithInteger:status] forKey:@"status"];
+    
+    [_alertViewOptions replaceObjectAtIndex:indexPath.row withObject:selectedCell];
+    
+    NSLog(@"For: %@ Now Selected: %@", [selectedCell objectForKey:@"title"], [selection objectAtIndex:status]);
+    
+    [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObjects:indexPath, nil] withRowAnimation:UITableViewRowAnimationNone];
+    
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+}
+
+- (UIColor *) checkFlatColors:(NSString *)selectedColor {
+    
+    FCAlertView *alert = [[FCAlertView alloc] init]; // Ignore This FCAlertView, simply initalized for colors here
+    
+    UIColor *color;
+    
+    if ([selectedColor isEqual:@"Turquoise"])
+        color = alert.flatTurquoise;
+    if ([selectedColor isEqual:@"Green"])
+        color = alert.flatGreen;
+    if ([selectedColor isEqual:@"Blue"])
+        color = alert.flatBlue;
+    if ([selectedColor isEqual:@"Midnight"])
+        color = alert.flatMidnight;
+    if ([selectedColor isEqual:@"Purple"])
+        color = alert.flatPurple;
+    if ([selectedColor isEqual:@"Orange"])
+        color = alert.flatOrange;
+    if ([selectedColor isEqual:@"Red"])
+        color = alert.flatRed;
+    if ([selectedColor isEqual:@"Silver"])
+        color = alert.flatSilver;
+    if ([selectedColor isEqual:@"Gray"])
+        color = alert.flatGray;
+    if ([selectedColor isEqual:@"OFF"])
+        color = self.redColor;
+    
+    return color;
     
 }
 
@@ -180,6 +249,13 @@
 //    @property (weak, nonatomic) IBOutlet UIButton *closeButtonToggle;
     
     FCAlertView *alert = [[FCAlertView alloc] init];
+    
+    // SETTING ALERT COLOR SCHEME
+    
+    NSString *selectedColor = [[[_alertViewOptions objectAtIndex:0] objectForKey:@"selection"]
+                               objectAtIndex:[[[_alertViewOptions objectAtIndex:0] objectForKey:@"status"] integerValue]];
+    
+    alert.colorScheme = [self checkFlatColors:selectedColor];
     
     // SETTING ALERT TYPE
     
